625bf5380b5e6f2e347a3bd4599909fbfd0cc332
Susie C <susie.y123@hotmail.co.uk>
2022-06-29 10:55:06 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -8,12 +8,12 @@ USE MyMovieList;
 
 CREATE TABLE movies (
 	movie_id INT NOT NULL AUTO_INCREMENT,
-    movie_title VARCHAR(250) NOT NULL,
-    release_year VARCHAR(4),
-    runtime_minutes SMALLINT NOT NULL, 
-    date_added DATE NOT NULL,
-    my_rating TINYINT NOT NULL,
-    PRIMARY KEY (movie_id)
+   	movie_title VARCHAR(250) NOT NULL,
+    	release_year VARCHAR(4),
+   	runtime_minutes SMALLINT NOT NULL, 
+    	date_added DATE NOT NULL,
+    	my_rating TINYINT NOT NULL,
+    	PRIMARY KEY (movie_id)
 );
 
 SELECT * FROM movies;
@@ -22,8 +22,8 @@ SELECT * FROM movies;
 
 CREATE TABLE directors (
 	director_id INT NOT NULL AUTO_INCREMENT,
-    director_name VARCHAR(50) NOT NULL,
-    PRIMARY KEY (director_id)
+    	director_name VARCHAR(50) NOT NULL,
+    	PRIMARY KEY (director_id)
 );
 
 SELECT * FROM directors;
@@ -32,8 +32,8 @@ SELECT * FROM directors;
 
 CREATE TABLE genres (
 	genre_id INT NOT NULL AUTO_INCREMENT,
-    genre VARCHAR(50) NOT NULL,
-    PRIMARY KEY (genre_id)
+    	genre VARCHAR(50) NOT NULL,
+    	PRIMARY KEY (genre_id)
 );
 
 SELECT * FROM genres;
@@ -42,10 +42,10 @@ SELECT * FROM genres;
 
 CREATE TABLE movie_director (
 	movie_id INT NOT NULL,
-    director_id INT NOT NULL,
-    CONSTRAINT pk_movie_director PRIMARY KEY (director_id, movie_id),
-    CONSTRAINT fk_movie_director_id FOREIGN KEY (director_id) REFERENCES directors (director_id),
-    CONSTRAINT fk_movie_movie_id FOREIGN KEY (movie_id) REFERENCES movies (movie_id)
+    	director_id INT NOT NULL,
+    	CONSTRAINT pk_movie_director PRIMARY KEY (director_id, movie_id),
+    	CONSTRAINT fk_movie_director_id FOREIGN KEY (director_id) REFERENCES directors (director_id),
+    	CONSTRAINT fk_movie_movie_id FOREIGN KEY (movie_id) REFERENCES movies (movie_id)
 );
 
 SELECT * FROM movie_director;
@@ -54,10 +54,10 @@ SELECT * FROM movie_director;
 
 CREATE TABLE movie_genre (
 	movie_id INT NOT NULL,
-    genre_id INT NOT NULL,
-    CONSTRAINT pk_movie_genre PRIMARY KEY (genre_id, movie_id),
-    CONSTRAINT fk_movie_genre_genre_id FOREIGN KEY (genre_id) REFERENCES genres (genre_id),
-    CONSTRAINT fk_movie_genre_movie_id FOREIGN KEY (movie_id) REFERENCES movies (movie_id)
+    	genre_id INT NOT NULL,
+    	CONSTRAINT pk_movie_genre PRIMARY KEY (genre_id, movie_id),
+    	CONSTRAINT fk_movie_genre_genre_id FOREIGN KEY (genre_id) REFERENCES genres (genre_id),
+    	CONSTRAINT fk_movie_genre_movie_id FOREIGN KEY (movie_id) REFERENCES movies (movie_id)
 );
 
 SELECT * FROM movie_genre;
